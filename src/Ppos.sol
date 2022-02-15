adb8188f7928ac603857f87ae9191f949e3ee4c5
platocrat <jack.abe.winfield@gmail.com>
2022-02-14 21:27:19 -0800

    clean up test ctc

--- src/Ppos.sol
+++ src/Ppos.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.10;
 
-contract Ppos {
-}
+import "./Sweetgreen/libraries/SgMenu__Spring2022.sol";
+
+contract PPoS {}
--- src/Ppos.t.sol
+++ src/Ppos.t.sol
@@ -2,14 +2,17 @@
 pragma solidity ^0.8.6;
 
 import "ds-test/test.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "./Ppos.sol";
+import "./Sweetgreen/libraries/SgMenu__Spring2022.sol";
+import "./PPoS.sol";
 
-contract PposTest is DSTest {
-    Ppos ppos;
+contract PPoSTest is DSTest {
+    PPoS ppos;
+    address constant USDC_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     function setUp() public {
-        ppos = new Ppos();
+        ppos = new PPoS();
     }
 
     function testFail_basic_sanity() public {
@@ -18,5 +21,16 @@ contract PposTest is DSTest {
 
     function test_basic_sanity() public {
         assertTrue(true);
+    }
+
+    function sendOrder() public {}
+
+    function testTransferSomething() public {
+        // We call `.balanceOf()` on the  contract
+        address sender = msg.sender;
+        address recipient = 0x144579C1d14335580A4F7DcF14554099a30074c9;
+
+        uint256 senderBalance = ERC20(USDC_ADDRESS).balanceOf(sender);
+        uint256 recipientBalance = ERC20(USDC_ADDRESS).balanceOf(recipient);
     }
 }
